e1ce99c2d1ea4e475579bc73d995d74ddbdfe193
duncte123 <contact@duncte123.me>
2020-06-29 22:47:19 +0200

    The bundle is broken

--- src/owoify.js
+++ src/owoify.js
@@ -1,8 +1,3 @@
 const owoify = require('owoify-js');
 
-if (process.argv.length < 3) {
-  console.log('Nope');
-  return process.exit(-1);
-}
-
 console.log(owoify.default(process.argv[3], process.argv[2]));
