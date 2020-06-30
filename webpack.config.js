520d14ac9627c240296ed8d0305c17ce20959b0e
duncte123 <contact@duncte123.me>
2020-06-30 09:06:37 +0200

    Add automated build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 
-module.exports = {
-  target: 'node',
-  mode: 'production',
-  entry: './src/owoify.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'owoify.bundle.js',
-  },
+module.exports = (env) => {
+  return {
+    target: 'node',
+    mode: env.MODE,
+    entry: './src/owoify.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'owoify.bundle.js',
+    },
+  };
 };
